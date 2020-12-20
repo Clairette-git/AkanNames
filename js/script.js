94d4e7734254efbee224612a097126cf31367d33
Clairette-git <marcletty12@gmail.com>
2020-12-20 22:41:25 +0200

    declaring variables of names and days in javascript

--- js/script.js
+++ js/script.js
@@ -3,4 +3,8 @@ function naming(){
     var YY=parseInt(document.getElementById("name").value);
     var MM=parseInt(document.getElementById("Form").value);
     var DD=parseInt(document.getElementById("akan").value);
+
+    var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
+    var femaleNames=["Akosua", "Adwoa","Abenaa","Akua","Yaa"," Afua","Ama"];
+    var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 }
