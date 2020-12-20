16f9a91f0b3752dd22eaca296cea9472c96434d3
Clairette-git <marcletty12@gmail.com>
2020-12-20 22:36:41 +0200

    declaring variables in javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 function naming(){
-
+    var CC=parseInt(document.getElementById("main").value);
+    var YY=parseInt(document.getElementById("name").value);
+    var MM=parseInt(document.getElementById("Form").value);
+    var DD=parseInt(document.getElementById("akan").value);
 }
